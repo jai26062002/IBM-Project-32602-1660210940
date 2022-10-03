021bf3f3b64296382bc4ab9a6524cf67f6c81c03
kaarthika23 <114347214+kaarthika23@users.noreply.github.com>
2022-10-03 09:43:53 +0530

    Delete script.js

--- Karthiga Priyadharshini - Team member 3/script.js
+++
@@ -1,6 +0,0 @@
-var btnLogin = document.getElementById('do-login');
-var idLogin = document.getElementById('login');
-var username = document.getElementById('username');
-btnLogin.onclick = function(){
-  idLogin.innerHTML = '<p>\ Logged in </p> <h1>' ;'</h1>';
-}
